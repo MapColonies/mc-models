450a2ea35944631124f032f0e751876446f7f0e2
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-08-03 09:48:56 +0300

    style(config): added missing ;

--- mc-nest-schema-validator/src/config.ts
+++ mc-nest-schema-validator/src/config.ts
@@ -2,7 +2,8 @@ import { has, get } from 'config';
 
 export type Logger = {
   log: (level: string, message: string) => void;
-}
+};
+
 export class Config {
   public static schemaSourceRoot: string = has('validation.schemaSourceRoot')
     ? get<string>('validation.schemaSourceRoot')
