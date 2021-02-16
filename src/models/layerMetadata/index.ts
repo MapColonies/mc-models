8f4a1d9fdedcf278953bdea3e9f3df287dabacab
Alex Lebinson <newdarkvirus@gmail.com>
2021-02-16 16:28:18 +0200

    feat(unit-tests): implemented and integrated (#37)
    * feat(unit-tests): implemented and integrated
    
    * fix(pr-comments): fixed

--- src/models/layerMetadata/index.ts
+++ src/models/layerMetadata/index.ts
@@ -1,2 +1,5 @@
 export * from './layerMetadata';
 export * from './layerMetadata-StatusFields';
+
+export { IShpMapping } from './decorators/shp.decorator';
+export { IPYCSWMapping } from './decorators/csw.decorator';
