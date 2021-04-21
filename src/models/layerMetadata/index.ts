6cc98b9e2a5acbe0a412ed39f4b03a00a4aa6b4a
Shlomi Koncha <cl_shlomikoncha@rnd-hub.com>
2021-04-21 10:56:27 +0300

    chore: fixed imports

--- src/models/layerMetadata/index.ts
+++ src/models/layerMetadata/index.ts
@@ -5,4 +5,4 @@ export { IShpMapping, ShapeFileType } from './decorators/property/shp.decorator'
 export { TsTypes } from './decorators/property/tsTypes.decorator';
 export { IPYCSWMapping } from './decorators/property/csw.decorator';
 export { IColumnProps } from './decorators/property/catalogDB.decorator';
-export { IOrmCatalog } from '../common/interfaces/IOrmCatalog';
+export { IOrmCatalog } from '../common/interfaces/ormCatalog.interface';
