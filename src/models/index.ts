0f700147333c925d6448cd9a7d4864baa4812edc
Shahar s <65118046+cl-shaharshavit-rnd@users.noreply.github.com>
2021-06-07 15:36:13 +0300

    feat!: updated yamls for catalog (#72)

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from './common/index';
 export * from './layerMetadata/index';
 export * from './discreteIngestion/index';
 export * from './pycsw/index';
+export * from './rasterCatalog/index';
