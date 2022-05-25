9d1c0f4e52489f9b57b785b3b2cd7e6a75e61908
Asaf <asaf.masa@gmail.com>
2022-05-25 11:59:41 +0300

    feat: add ORM columns types (#129)
    * feat: add ORM columns enum
    
    * feat: add type export to index
    
    * fix: fix lint
    
    * fix: fix cr comments
    
    Co-authored-by: Asaf Masa <>

--- src/models/layerMetadata/decorators/property/catalogDB.decorator.ts
+++ src/models/layerMetadata/decorators/property/catalogDB.decorator.ts
@@ -18,8 +18,21 @@ export interface IFieldProps {
   overrideType?: IDescribeTsType;
 }
 
+export enum ORMColumnType {
+  COLUMN = 'Column',
+  CREATE_DATE_COLUMN = 'CreateDateColumn',
+  DELETE_DATE_COLUMN = 'DeleteDateColumn',
+  OBJECT_ID_COLUMN = 'ObjectIdColumn',
+  PRIMARY_COLUMN = 'PrimaryColumn',
+  PRIMARY_GENERATED_COLUMN = 'PrimaryGeneratedColumn',
+  UPDATE_DATE_COLUMN = 'UpdateDateColumn',
+  VERSION_COLUMN = 'VersionColumn',
+  VIEW_COLUMN = 'ViewColumn',
+}
+
 export interface ICatalogDBMapping {
   column: IColumnProps; // column properties
+  columnType?: ORMColumnType; // deafult is 'Column'
   field?: IFieldProps; // field properties
 }
 
