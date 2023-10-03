621440045fc7576ed6041b21936fa21d59f56c7c
ronenk1 <ronenk1@rnd-hub.com>
2023-10-03 14:13:12 +0300

    fix: naming - format to tilesMimeFormat (layerMetadata)

--- src/models/layerMetadata/layerMetadata.ts
+++ src/models/layerMetadata/layerMetadata.ts
@@ -1115,7 +1115,7 @@ export class LayerMetadata implements ILayerMetadata, IMetadataCommonModel {
     mappingType: TsTypes.STRING,
   })
   //#endregion
-  public format: TilesMimeFormat | undefined = undefined;
+  public tilesMimeFormat: TilesMimeFormat | undefined = undefined;
 
   @catalogDB({
     column: {
