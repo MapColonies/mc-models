d99d608ddcdc5924b427c6b0da22ee7b2efe554f
Ronit Kissis <92728393+RonitKissis@users.noreply.github.com>
2024-06-20 17:07:05 +0300

    fix: new raster ingestion modifications (MAPCO-4302) (#189)
    * fix: fix mandatory fields in new pp design
    
    * fix: parial fields
    
    * fix: polygonPart type
    
    * fix: partData schema
    
    * fix: geo in example
    
    * fix: changes
    
    * feat: base metadata
    
    * feat: description is not editable via put
    
    * feat: seperate requests
    
    * fix: removed comment
    
    * fix: public constructor
    
    * fix: example

--- src/models/raster/ingestion/ingestionTypes.ts
+++ src/models/raster/ingestion/ingestionTypes.ts
@@ -1,51 +1,53 @@
 import { LayerMetadata, ProductType, Transparency } from '../../layerMetadata';
 import { PolygonPartRecord } from '../../polygonParts';
 
-export type IUpdateRasterLayerMetadata = Pick<LayerMetadata, 'productSubType' | 'description' | 'region' | 'classification' | 'scale'>;
+export type IBaseRasterLayerMetadata = Pick<LayerMetadata, 'classification'>;
 
-export class UpdateRasterLayerMetadata implements IUpdateRasterLayerMetadata {
-  public productSubType: string | undefined;
-  public description: string | undefined;
-  public region: string[] | undefined;
-  public classification: string | undefined;
-  public scale: number | undefined;
+export class BaseRasterLayerMetadata implements IBaseRasterLayerMetadata {
+  public classification: string;
 
-  public constructor(productSubType?: string, description?: string, region?: string[], classification?: string, scale?: number) {
-    this.productSubType = productSubType;
-    this.description = description;
-    this.region = region;
+  public constructor(classification: string) {
     this.classification = classification;
-    this.scale = scale;
+  }
+}
+export class UpdateRasterLayerMetadata extends BaseRasterLayerMetadata {
+  public constructor(classification: string) {
+    super(classification);
   }
 }
 
-export type INewRasterLayerMetadata = UpdateRasterLayerMetadata &
-  Pick<LayerMetadata, 'productId' | 'producerName' | 'productType' | 'srs' | 'srsName' | 'transparency' | 'productName'>;
+export type INewRasterLayerMetadata = BaseRasterLayerMetadata &
+  Pick<LayerMetadata, 'productId' | 'productType' | 'srs' | 'srsName' | 'transparency' | 'productName'> &
+  Partial<Pick<LayerMetadata, 'producerName' | 'description'>>;
 
-export class NewRasterLayerMetadata extends UpdateRasterLayerMetadata implements INewRasterLayerMetadata {
-  public productId: string | undefined;
-  public producerName: string | undefined;
-  public productType: ProductType | undefined;
-  public srs: string | undefined;
-  public srsName: string | undefined;
-  public transparency: Transparency | undefined;
-  public productName: string | undefined;
+export class NewRasterLayerMetadata extends BaseRasterLayerMetadata implements INewRasterLayerMetadata {
+  public productId: string;
+  public productType: ProductType;
+  public srs: string;
+  public srsName: string;
+  public transparency: Transparency;
+  public productName: string;
+  public region: string[];
+  public productSubType?: string;
+  public scale?: number;
+  public producerName?: string;
+  public description?: string;
 
   public constructor(
-    productId?: string,
-    producerName?: string,
-    productType?: ProductType,
-    srs?: string,
-    srsName?: string,
-    transparency?: Transparency,
-    productName?: string,
+    productId: string,
+    productType: ProductType,
+    srs: string,
+    srsName: string,
+    transparency: Transparency,
+    productName: string,
+    region: string[],
+    classification: string,
     productSubType?: string,
     description?: string,
-    region?: string[],
-    classification?: string,
-    scale?: number
+    scale?: number,
+    producerName?: string
   ) {
-    super(productSubType, description, region, classification, scale);
+    super(classification);
     this.productId = productId;
     this.producerName = producerName;
     this.productType = productType;
@@ -53,33 +55,36 @@ export class NewRasterLayerMetadata extends UpdateRasterLayerMetadata implements
     this.srsName = srsName;
     this.transparency = transparency;
     this.productName = productName;
+    this.scale = scale;
+    this.productSubType = productSubType;
+    this.region = region;
+    this.description = description;
   }
 }
 
 export type PolygonPart = Pick<
   PolygonPartRecord,
-  | 'id'
   | 'name'
-  | 'description'
   | 'resolutionDegree'
   | 'resolutionMeter'
   | 'sourceResolutionMeter'
   | 'horizontalAccuracyCE90'
-  | 'countries'
-  | 'cities'
   | 'sensors'
   | 'imagingTimeBeginUTC'
   | 'imagingTimeEndUTC'
   | 'geometry'
->;
+> &
+  Partial<Pick<PolygonPartRecord, 'id' | 'description' | 'countries' | 'cities'>>;
 
 export interface InputFiles {
   originDirectory: string;
   fileNames: string[];
 }
 
-export interface RasterIngestionLayer {
-  metadata: NewRasterLayerMetadata | UpdateRasterLayerMetadata;
+export interface LayerData {
   partData: PolygonPart[];
   inputFiles: InputFiles;
 }
+
+export type NewRasterLayer = { metadata: NewRasterLayerMetadata } & LayerData;
+export type UpdateRasterLayer = { metadata: UpdateRasterLayerMetadata } & LayerData;
