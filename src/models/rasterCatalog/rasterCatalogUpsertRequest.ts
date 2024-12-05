77676285fea10823ad0b3741caa307f0a093ca4f
Ronit Kissis <92728393+RonitKissis@users.noreply.github.com>
2024-12-05 10:29:55 +0200

    fix: add keywords to editable fields(MAPCO-5673) (#244)
    * fix: add keywords to editable fields
    
    * fix: name change

--- src/models/rasterCatalog/rasterCatalogUpsertRequest.ts
+++ src/models/rasterCatalog/rasterCatalogUpsertRequest.ts
@@ -1,8 +1,11 @@
 import { Link } from '../common';
-import { LayerMetadata } from '../layerMetadata';
+import { LayerMetadata, PycswLayerCatalogRecord } from '../layerMetadata';
 
-export type UpdateLayerMetadata = Partial<
-  Pick<LayerMetadata, 'classification' | 'productName' | 'productSubType' | 'description' | 'producerName' | 'region' | 'scale'>
+export type EditLayerMetadata = Partial<
+  Pick<
+    PycswLayerCatalogRecord,
+    'classification' | 'productName' | 'productSubType' | 'description' | 'producerName' | 'region' | 'scale' | 'keywords'
+  >
 >;
 
 export interface IRasterCatalogUpsertRequestBody {
@@ -11,5 +14,5 @@ export interface IRasterCatalogUpsertRequestBody {
 }
 
 export interface IRasterCatalogEditRequestBody {
-  metadata: UpdateLayerMetadata;
+  metadata: EditLayerMetadata;
 }
