fb4e072b200631cb9f42241ea4a453a363194eec
almog8k <almog8k@gmail.com>
2025-01-29 11:00:11 +0200

    chore: use raster types from raster-shared, use common types from types pacage

--- src/models/layerMetadata/vectorBestMetadata.ts
+++ src/models/layerMetadata/vectorBestMetadata.ts
@@ -1,4 +1,5 @@
 import { GeoJSON } from 'geojson';
+import { RecordType } from '@map-colonies/types';
 import { IPropCatalogDBMapping } from '../common/interfaces/propCatalogDBMapping.interface';
 import { graphql } from '../common/decorators/graphQL/graphql.decorator';
 import {
@@ -8,7 +9,6 @@ import {
   IFieldConfigInfo,
   IPropFieldConfigInfo,
 } from '../common/decorators/fieldConfig/fieldConfig.decorator';
-import { RecordType } from '../pycsw/coreEnums';
 import { getPyCSWMapping, IPYCSWMapping, pycsw } from './decorators/property/csw.decorator';
 import { getCatalogDBMapping, ICatalogDBMapping, catalogDB } from './decorators/property/catalogDB.decorator';
 import { getTsTypesMapping, tsTypes, TsTypes } from './decorators/property/tsTypes.decorator';
